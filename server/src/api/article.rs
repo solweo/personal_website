3b99196152211b611a1b21c891f2950be75e3ebf
solweo <60581773+solweo@users.noreply.github.com>
2024-05-23 22:11:20 +0300

    Markdown parser substitution

--- server/src/api/article.rs
+++ server/src/api/article.rs
@@ -3,10 +3,10 @@ use leptos_meta::*;
 use leptos_router::*;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
-use pulldown_cmark::{html as pull_mark, Options, Parser};
+// use comrak::{markdown_to_html, Options};
+use comrak::*;
 
-static MARKDOWN_SOURCE: &str = r#"
----                                                                            
+static MARKDOWN_SOURCE: &str = r#"---                                                                            
 title: 'Some title'                                                         
 description: 'Some description'     
 aliases: ['some alias', 'another alias']          
@@ -74,13 +74,68 @@ pub enum ArticleError {
 pub async fn fetch_article(id: i32) -> Result<Article, ServerFnError> {
     println!("Called `fetch_article`");
 
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_TABLES);
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    options.insert(Options::ENABLE_TASKLISTS);
-    let parser = pulldown_cmark::Parser::new(MARKDOWN_SOURCE);
-    let mut html_output = String::new();
-    pull_mark::push_html(&mut html_output, parser);
+    // let mut options = Options::empty();
+    // options.insert(Options::ENABLE_TABLES);
+    // options.insert(Options::ENABLE_STRIKETHROUGH);
+    // options.insert(Options::ENABLE_TASKLISTS);
+    // let parser = pulldown_cmark::Parser::new(MARKDOWN_SOURCE);
+    // let mut html_output = String::new();
+    // pull_mark::push_html(&mut html_output, parser);
+
+    // comrak
+
+    // let a = ExtensionOptions {
+    //     strikethrough: true,
+    //     tagfilter: true,
+    //     table: true,
+    //     autolink: true,
+    //     tasklist: true,
+    //     superscript: true,
+    //     header_ids: true,
+    //     footnotes: true,
+    //     description_lists: true,
+    //     front_matter_delimiter: true,
+    //     multiline_block_quotes: true,
+    //     math_dollars: true,
+    //     math_code: true,
+    //     wikilinks_title_after_pipe: true,
+    //     wikilinks_title_before_pipe: true,
+    // };
+
+    let extension = ExtensionOptionsBuilder::default()
+        .strikethrough(true)
+        .tagfilter(true)
+        .table(true)
+        .autolink(true)
+        .tasklist(true)
+        .superscript(true)
+        .header_ids(Some("user-content-".to_string()))
+        .footnotes(true)
+        .description_lists(true)
+        .front_matter_delimiter(Some("---".to_owned()))
+        .multiline_block_quotes(true)
+        .math_dollars(true)
+        .math_code(true)
+        .wikilinks_title_after_pipe(true)
+        .wikilinks_title_before_pipe(true)
+        .build()
+        .unwrap();
+
+    let parse = ParseOptionsBuilder::default()
+        .smart(true)
+        .default_info_string(Some("rust".to_string()))
+        .relaxed_tasklist_matching(true)
+        .relaxed_autolinks(true)
+        .build()
+        .unwrap();
+
+    let options = Options {
+        extension,
+        parse,
+        ..Options::default()
+    };
+
+    let html_output = markdown_to_html(MARKDOWN_SOURCE, &options);
 
     Ok(Article {
         id: "yuvOBfTQ_bw".to_string(),
