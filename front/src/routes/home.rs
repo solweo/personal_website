0a7c209115d51f921ed0619b14e4ddefd234f399
solweo <60581773+solweo@users.noreply.github.com>
2024-08-29 11:31:08 +0300

    Re-do of tagline, but now with a typewriter effect

--- front/src/routes/home.rs
+++ front/src/routes/home.rs
@@ -1,8 +1,10 @@
 use leptos::*;
+use ui_kit::Tagline;
 
 #[component]
 pub fn Home() -> impl IntoView {
     view! {
         <h3>"Home page placeholder"</h3>
+        <Tagline/>
     }
 }
--- ui_kit/src/lib.rs
+++ ui_kit/src/lib.rs
@@ -1,2 +1,6 @@
+pub mod tagline;
+
+pub use tagline::*;
+
 use stylance::import_style as get_css;
 get_css!(pub style_baseline, "./index.css");
