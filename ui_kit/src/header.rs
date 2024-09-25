179fb0aa92651c3694474f4591a16b7c7da59a48
solweo <60581773+solweo@users.noreply.github.com>
2024-09-25 12:18:18 +0300

    Header draft: styles & sections

--- ui_kit/src/header.rs
+++ ui_kit/src/header.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos_router::*;
 use stylance::import_style as get_css;
 
 get_css!(header_css, "./header.css");
@@ -6,6 +7,29 @@ get_css!(header_css, "./header.css");
 #[component]
 pub fn Header() -> impl IntoView {
     view! {
-        <p>"Header placeholder"</p>
+        <header 
+            class=header_css::bottom_floating_header 
+        >
+            <A 
+                class=header_css::header_nav
+                href="/"
+            >"Solweo"</A>
+            <A 
+                class=header_css::header_nav
+                href="/about"
+            >"About"</A>
+            <A 
+                class=header_css::header_nav
+                href="/works"
+            >"Works"</A>
+            <A 
+                class=header_css::header_nav
+                href="/playground"
+            >"Playground"</A>
+            <A 
+                class=header_css::header_nav
+                href="/"
+            >"Contact"</A>
+        </header>
     }
 }
--- ui_kit/src/typewritter.rs
+++ ui_kit/src/typewritter.rs
@@ -13,63 +13,6 @@ enum HoldupCueWaitBefore {
 }
 
 #[component]
-pub fn OldTypeWritterFX(words: Vec<&'static str>) -> impl IntoView {
-    let type_buffer = create_rw_signal(("", HoldupCueWaitBefore::Start));
-    let typed = create_rw_signal("");
-    let show_stub = create_rw_signal(true);
-    
-    let start_delay = Duration::from_millis(300);
-    let typing_delay = Duration::from_millis(90);
-    let empty_delay = Duration::from_millis(900);
-    
-    let mut result = Vec::new();
-    // for word in ["tech artistry", "art sorcery", "product wizardy"].iter() {
-    for word in words.iter() {
-        for i in 0..=word.len() {
-            let cue = if i == 0 {
-                HoldupCueWaitBefore::Start
-            } else if i < word.len() {
-                HoldupCueWaitBefore::Type
-            } else {
-                HoldupCueWaitBefore::Empty
-            };
-            result.push((&word[..i], cue));
-        }
-    }
-    let it = store_value(result.into_iter().cycle());
-    
-    let handle: StoredValue<Option<TimeoutHandle>> = store_value(None);
-
-    create_render_effect(move |_| {
-        if let Some(h) = handle.get_value() {
-            h.clear();
-        }
-
-        let delay = {
-            let (line, cue) = type_buffer.get();
-            typed.set(line);
-            match cue {
-                HoldupCueWaitBefore::Start => {show_stub.set(true); start_delay},
-                HoldupCueWaitBefore::Type => typing_delay,
-                HoldupCueWaitBefore::Empty => {show_stub.set(false); empty_delay},
-            }
-        };
-
-        handle.set_value(Some(leptos_dom::helpers::set_timeout_with_handle(
-            move || {
-                it.update_value(|v| type_buffer.set(v.next().unwrap()));
-            },
-            delay,
-        ).expect("set timeout in TypeWritterFX")));
-        
-    });
-
-    view! {
-        <span class=typewritter_css::liquid>"[ "{typed}<Stub show=show_stub/>" ]"</span>
-    }
-}
-
-#[component]
 pub fn TypeWritterFX(words: Vec<String>) -> impl IntoView {
     let type_buffer = create_rw_signal((String::new(), HoldupCueWaitBefore::Start));
     let typed = create_rw_signal(String::new());
@@ -122,73 +65,6 @@ pub fn TypeWritterFX(words: Vec<String>) -> impl IntoView {
 
     view! {
         <span class=typewritter_css::liquid>"[ "{typed}<Stub show=show_stub/>" ]"</span>
-    }
-}
-
-#[component]
-pub fn ModTypeWritterFX(words: Vec<String>) -> impl IntoView {
-    let type_buffer = create_rw_signal((String::new(), HoldupCueWaitBefore::Start));
-    let typed = create_rw_signal(String::new());
-    let show_stub = create_rw_signal(true);
-    
-    let start_delay = Duration::from_millis(300);
-    let typing_delay = Duration::from_millis(90);
-    let empty_delay = Duration::from_millis(900);
-    
-    let mut result = Vec::new();
-    // for word in ["tech artistry", "art sorcery", "product wizardy"].iter() {
-
-    // let a = words.iter().map(|w| w.as_str().clone())
-
-    // let words = words.iter().map(String::as_str).collect::<Vec<_>>();
-    // let words = words.clone();
-    // use leptos_reactive::store_value;
-    // let words = store_value(words.clone());
-    // let words = words.iter().cloned().collect::<Vec<_>>();
-
-    for word in words.iter() {
-        for i in 0..=word.len() {
-            let cue = if i == 0 {
-                HoldupCueWaitBefore::Start
-            } else if i < word.len() {
-                HoldupCueWaitBefore::Type
-            } else {
-                HoldupCueWaitBefore::Empty
-            };
-            result.push((word[..i].to_string(), cue));
-        }
-    }
-
-    let it = store_value(result.into_iter().cycle());
-    
-    let handle: StoredValue<Option<TimeoutHandle>> = store_value(None);
-
-    create_render_effect(move |_| {
-        if let Some(h) = handle.get_value() {
-            h.clear();
-        }
-
-        let delay = {
-            let (line, cue) = type_buffer.get();
-            typed.set(line);
-            match cue {
-                HoldupCueWaitBefore::Start => {show_stub.set(true); start_delay},
-                HoldupCueWaitBefore::Type => typing_delay,
-                HoldupCueWaitBefore::Empty => {show_stub.set(false); empty_delay},
-            }
-        };
-
-        handle.set_value(Some(leptos_dom::helpers::set_timeout_with_handle(
-            move || {
-                it.update_value(|v| type_buffer.set(v.next().unwrap()));
-            },
-            delay,
-        ).expect("set timeout in TypeWritterFX")));
-        
-    });
-
-    view! {
-        <h1><span class=typewritter_css::liquid>"[ "{typed}<Stub show=show_stub/>" ]"</span></h1> 
     }
 }
 
