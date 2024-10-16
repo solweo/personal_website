56a75abe5d698a742af90d9ad6a1d8e7685d24b1
solweo <60581773+solweo@users.noreply.github.com>
2024-10-16 20:19:35 +0300

    Restructuring article's css for the sake of restructure

--- server/src/api/fetch_preview.rs
+++ server/src/api/fetch_preview.rs
@@ -24,6 +24,45 @@ pub async fn fetch_preview() -> Result<Vec<ArticlePreview>, ServerFnError<ApiErr
 
     let mut meta_bodies = Vec::new();
 
+    // Just dummy data
+    meta_bodies.append(&mut vec![
+        ArticlePreview {
+            id: "akd1f32".to_string(),
+            aliases: vec!["How to make your protocol".to_string()],
+            description: "some description".to_string(),
+            preview: "http://cdn.solweo.tech/assets/church.jpg".to_string(),
+            liasions: vec!["layout system".to_string()],
+        },
+        ArticlePreview {
+            id: "ae02agp".to_string(),
+            aliases: vec!["Life in Cyprus (+ photos)".to_string()],
+            description: "some description".to_string(),
+            preview: "http://cdn.solweo.tech/assets/discourse.jpg".to_string(),
+            liasions: vec!["layout system".to_string()],
+        },
+        ArticlePreview {
+            id: "2fmi8ht".to_string(),
+            aliases: vec!["Future of web engines and web itself".to_string()],
+            description: "some description".to_string(),
+            preview: "http://cdn.solweo.tech/assets/dreming.jpg".to_string(),
+            liasions: vec!["layout system".to_string()],
+        },
+        ArticlePreview {
+            id: "e14ar2".to_string(),
+            aliases: vec!["Implementing SSR in Leptos".to_string()],
+            description: "some description".to_string(),
+            preview: "http://cdn.solweo.tech/assets/eating.jpg".to_string(),
+            liasions: vec!["layout system".to_string()],
+        },
+        ArticlePreview {
+            id: "ga56sd".to_string(),
+            aliases: vec!["Taking notes is only underset of memex system".to_string()],
+            description: "some description".to_string(),
+            preview: "http://cdn.solweo.tech/assets/flower.jpg".to_string(),
+            liasions: vec!["layout system".to_string()],
+        }
+    ]);
+
     for id in ids {
         let response = reqwest::get(format!("http://cdn.solweo.tech/work/{}/en.md", id)).await;
         if let Ok(response) = response { 
